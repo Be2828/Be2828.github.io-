0960847037d2f1a9672037f978b65bb289689735
Be2828 <126707636+Be2828@users.noreply.github.com>
2023-03-07 23:42:05 -0300

    Initial commit

---
+++ curriculum.js
@@ -0,0 +1,26 @@
+var numero = 8929;
+var calle = "Valwood Pkwy";
+var ciudad = "Billings";
+var estado = "Michigan";
+var país = "Estados Unidos";
+var zipcode = 63104;
+
+var concatenacion = numero + calle;
+
+var datos = document.getElementById ("datos");
+
+
+
+
+
+
+
+
+
+
+
+
+      
+        
+       
+      
